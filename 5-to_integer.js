ddde0fc699b3116e2acd67b1ca267876a7236bca
kweku-annan <kwekuannan.dev@gmail.com>
2025-11-09 20:21:27 +0000

    Correct mistakes in 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,7 +4,7 @@ arg = argv[2];
 const num = parseInt(arg);
 
 if (!isNaN(num)) {
-    console.log("My number: ${num}");
+    console.log(`My number: ${num}`);
 } else {
     console.log("Not a number");
 }
