703eaf203e7089df4f12df6affd97af0307eb01f
kweku-annan <kwekuannan.dev@gmail.com>
2025-11-09 20:18:37 +0000

    Add 5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,10 @@
+const { argv } = require('node:process');
+arg = argv[2];
+
+const num = parseInt(arg);
+
+if (!isNaN(num)) {
+    console.log("My number: ${num}");
+} else {
+    console.log("Not a number");
+}
