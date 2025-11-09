51c8523c87699daab0843bd2b27800e0f2c8614f
kweku-annan <kwekuannan.dev@gmail.com>
2025-11-09 20:32:53 +0000

    Add 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+const languages = ["C", "Python", "JavaScript"];
+const descriptions = ["fun", "cool", "amazing"];
+
+for (let i = 0; i < 3; i++) {
+    console.log(`${languages[i]} is ${descriptions[i]}`);
+}
